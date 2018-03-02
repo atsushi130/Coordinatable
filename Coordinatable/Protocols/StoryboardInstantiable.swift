e5cfb8d4224ee07cbd27d7337feee2a2dd1c3a11
atsushi130 <atsushi_main@outlook.jp>
2018-03-02 09:23:12 +0900

    define: StoryboardInstantiable

---
+++ Coordinatable/Protocols/StoryboardInstantiable.swift
@@ -0,0 +1,49 @@
+//
+//  StoryboardInstantiable.swift
+//  Coordinatable
+//
+//  Created by Atsushi Miyake on 2018/03/02.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import UIKit
+
+enum StoryboardInstantiateType {
+    case initial
+    case identifier(String)
+}
+
+protocol StoryboardInstantiatable {
+    static var storyboardName: String { get }
+    static var storyboardBundle: Bundle { get }
+    static var instantiateType: StoryboardInstantiateType { get }
+}
+
+extension StoryboardInstantiatable where Self: NSObject {
+    static var storyboardName: String {
+        return className
+    }
+    
+    static var storyboardBundle: Bundle {
+        return Bundle(for: self)
+    }
+    
+    private static var storyboard: UIStoryboard {
+        return UIStoryboard(name: storyboardName, bundle: storyboardBundle)
+    }
+    
+    static var instantiateType: StoryboardInstantiateType {
+        return .identifier(className)
+    }
+}
+
+extension StoryboardInstantiatable where Self: UIViewController {
+    static func instantiate() -> Self {
+        switch instantiateType {
+        case .initial:
+            return storyboard.instantiateInitialViewController() as! Self
+        case .identifier(let identifier):
+            return storyboard.instantiateViewController(withIdentifier: identifier) as! Self
+        }
+    }
+}
