a15de40c9431f3e412831f43b4e5839e4fba20d5
atsushi130 <atsushi_main@outlook.jp>
2018-03-02 09:22:52 +0900

    add: NSObject extension

---
+++ Coordinatable/Extensions/NSObject+.swift
@@ -0,0 +1,38 @@
+//
+//  NSObject+.swift
+//  Coordinatable
+//
+//  Created by Atsushi Miyake on 2018/03/02.
+//  Copyright © 2018年 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+protocol ClassNameProtocol {
+    static var className: String { get }
+    var className: String { get }
+}
+
+extension ClassNameProtocol {
+    static var className: String {
+        return String(describing: self)
+    }
+    
+    var className: String {
+        return type(of: self).className
+    }
+}
+
+extension NSObject: ClassNameProtocol {}
+
+extension NSObjectProtocol {
+    var describedProperty: String {
+        let mirror = Mirror(reflecting: self)
+        return mirror.children.map { element -> String in
+            let key = element.label ?? "Unknown"
+            let value = element.value
+            return "\(key): \(value)"
+            }
+            .joined(separator: "\n")
+    }
+}
